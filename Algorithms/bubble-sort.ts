92b0dcbb589bd04de9eb6d516a448796f2ced8c4
Zachary Temple <zachary.temple@agvance.net>
2021-07-30 11:50:37 -0500

    Optimize bubble sort. Delete unused directory.

--- Algorithms/bubble-sort.ts
+++ Algorithms/bubble-sort.ts
@@ -1,13 +1,21 @@
-export function bubbleSort(array: number[]): void {
+// add noSwaps for nearly sorted arrays
+export function bubbleSort(array: number[]): number[] {
+  let noSwaps: boolean;
+
   for (let outer = array.length; outer >= 1; outer--) {
+    noSwaps = true;
     for (let inner = 0; inner <= outer - 1; inner++) {
       if (array[inner] > array[inner + 1]) {
         const temp = array[inner];
         array[inner] = array[inner + 1];
         array[inner + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
+
+  return array;
 }
 
 
