3795bea3c79c3630a27e7388a5541434d0638f9b
Zachary Temple <zachary.temple@agvance.net>
2021-08-01 10:40:58 -0500

    Add extract max to max binary heap

--- Structures/max-heap.ts
+++ Structures/max-heap.ts
@@ -6,6 +6,17 @@ export class MaxBinaryHeap {
     this.bubbleUp();
   }
 
+  public extractMax(): number {
+    if ( this.data.length === 0) return undefined;
+    const removedRoot = this.data[0];
+    this.data[0] = this.data[this.data.length - 1];
+    this.data.pop();
+
+    this.siftDown();
+
+    return removedRoot;
+  }
+
   private bubbleUp(): void {
     let index = this.data.length - 1;
     let newValue = this.data[index];
@@ -22,5 +33,30 @@ export class MaxBinaryHeap {
       parentIndex = Math.floor((index - 1) / 2);
       parent = this.data[parentIndex];
     }
+  }
+
+  private siftDown(): void {
+    let index = 0;
+    let leftChildIndex = (ind) => 2 * ind + 1
+    let leftChild = (ind) => this.data[leftChildIndex(ind)];
+    let rightChildIndex = (ind) => 2 * ind + 2
+    let rightChild = (ind) => this.data[rightChildIndex(ind)];
+
+    while (index < leftChild(index) || index < rightChild(index)) {
+      index = leftChild(index) > rightChild(index) ?
+        this.siftDownSwap(index, leftChildIndex(index), leftChild(index)) :
+        this.siftDownSwap(index, rightChildIndex(index), rightChild(index));
+    }
+  }
+
+  private siftDownSwap(idx: number, childIdx: number, childVal: number): number {
+    let temp;
+
+    temp = this.data[idx];
+    this.data[idx] = childVal;
+    this.data[childIdx] = temp;
+    idx = childIdx;
+
+    return idx
   }
 }
